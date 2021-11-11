8349646b59c854288fc4b7061c6811f22629349a
Denis Lipov <denlipov1@gmail.com>
2021-11-11 22:05:29 +0300

    Поправил тест repo, исключил магический код.

--- internal/repo/repo_test.go
+++ internal/repo/repo_test.go
@@ -8,19 +8,21 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/denlipov/com-request-api/internal/model"
+	pb "github.com/denlipov/com-request-api/pkg/com-request-api"
 	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
-func setupRepo(t *testing.T) (*repo, sqlmock.Sqlmock) {
+func newMockRepo(t *testing.T) (*repo, sqlmock.Sqlmock) {
 	mockDB, mock, _ := sqlmock.New()
 	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
 
-	r := &repo{
+	repo := &repo{
 		db: sqlxDB,
 	}
 
-	return r, mock
+	return repo, mock
 }
 
 type anyTime struct{}
@@ -31,24 +33,33 @@ func (a anyTime) Match(v driver.Value) bool {
 }
 
 func TestRemoveRequest_Success(t *testing.T) {
-	r, dbMock := setupRepo(t)
+	r, dbMock := newMockRepo(t)
 	ctx := context.Background()
+
+	requestID := uint64(1)
+	pbReq := &pb.Request{
+		Id: requestID,
+	}
+	payload, err := protojson.Marshal(pbReq)
+	if err != nil {
+		t.Fatalf("Failed to marshal protobuf.Request: %+v", err)
+	}
 
 	dbMock.ExpectBegin()
 	dbMock.ExpectExec("UPDATE requests").
-		WithArgs(true, anyTime{}, 1, false).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+		WithArgs(true, anyTime{}, requestID, false).
+		WillReturnResult(sqlmock.NewResult(int64(requestID), 1))
 
 	dbMock.ExpectExec("INSERT INTO requests_events").
-		WithArgs(1,
+		WithArgs(requestID,
 			model.Removed,
 			model.Idle,
-			[]byte{123, 34, 105, 100, 34, 58, 34, 49, 34, 125},
+			payload,
 			anyTime{}).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+		WillReturnResult(sqlmock.NewResult(int64(requestID), 1))
 	dbMock.ExpectCommit()
 
-	_, err := r.RemoveRequest(ctx, 1)
+	_, err = r.RemoveRequest(ctx, requestID)
 
 	require.NoError(t, err)
 }
